d42a4bb43f218c8c3c4bdf0be6e8a0c5cc9870e5
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-08-30 01:38:45 +0900

    刈った面積の計算とミニマップの修正

--- Assets/Scripts/AutoDestroyAndSpawnGrass.cs
+++ Assets/Scripts/AutoDestroyAndSpawnGrass.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class AutoDestroyAndSpawn : MonoBehaviour
 {
-    public float lifetime = 5f; // オブジェクトが破壊されるまでの時間（秒）
+    public float lifetime = 10.0f; // オブジェクトが生成されてから消滅するまでの時間
     public GameObject spawnPrefab; // 破壊後に生成されるオブジェクトのプレハブ
-
     private Vector3 initialPosition;
+    private GrassCounter counter;
 
     void Start()
+    {
+        // カウント用のスクリプトを取得
+        counter = FindObjectOfType<GrassCounter>();
+    }
+
+    void Update()
     {
         // コルーチンを開始して時間経過後にオブジェクトを破壊
         StartCoroutine(DestroyAfterLifetime());
@@ -21,12 +27,19 @@ public class AutoDestroyAndSpawn : MonoBehaviour
 
         // 指定された時間だけ待つ
         yield return new WaitForSeconds(lifetime);
-
         // オブジェクトを破壊
+        if (counter != null)
+        {
+            counter.IncrementCount();
+        }
         Destroy(gameObject);
-
         // 新しいオブジェクトを初期位置に生成
         initialPosition.y = 13.5f; // オブジェクトが地面に埋まらないように少し浮かせる    
-        Instantiate(spawnPrefab, initialPosition, Quaternion.Euler(270, 0, 0));
+        GameObject newObject = Instantiate(spawnPrefab, initialPosition, Quaternion.Euler(270, 0, 0));
+        // 新しいオブジェクトのカウントを増やす
+        if (newObject.TryGetComponent(out AutoDestroyAndSpawn newObjectScript))
+        {
+            newObjectScript.counter = counter;
+        }
     }
 }
