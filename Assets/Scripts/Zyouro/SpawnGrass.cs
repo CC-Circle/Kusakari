a28df2b5b5ea0537c50054c69fd57e0f4cfcf8b9
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-09-03 00:01:13 +0900

    草の生成

--- Assets/Scripts/Zyouro/SpawnGrass.cs
+++ Assets/Scripts/Zyouro/SpawnGrass.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpawnGrass : MonoBehaviour
 {
+    private int isCollide = 0;
+    public GameObject grass;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isCollide == 1)
+        {
+            Vector3 pos = transform.position;
+            pos.y = 13.5f;
+            Instantiate(grass, pos, Quaternion.Euler(270, 0, 0));
+            isCollide = 0;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "AfterGrass")
+        {
+            // Debug.Log("Playerが草に触れました");
+            Destroy(other.gameObject);
+            isCollide = 1;
+        }
     }
 }
